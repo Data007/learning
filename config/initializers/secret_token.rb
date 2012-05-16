fc976dd3d4e8d6b639829a828555aabee9f11eb3
Bryan Denslow <bryan@rebelhold.com>
2012-05-16 15:13:58 -0400

    fixed the project to use mongoid

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Learning::Application.config.secret_token = 'fed4c0ec38bbbe3f14f4115730d8cce3b4340dec03043a5ce4f56dbb942d6ab4782bc5f15b710d0344dd1265407f27b98e9fcf75351aff2c4bc7e72b37601129'
